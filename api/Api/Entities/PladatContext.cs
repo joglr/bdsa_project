cfdef6b50549aca541bc587e77a7891664a4cebd
emiljapelt <55390848+emiljapelt@users.noreply.github.com>
2020-11-30 15:00:51 +0100

    Setup Api to resemple BDSA assignments

--- api/Api/Entities/PladatContext.cs
+++ api/Api/Entities/PladatContext.cs
@@ -2,19 +2,30 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Api.Entities
 {
-    public class PladatContext : DbContext
+    public class PlaDatContext : DbContext
     {
         public DbSet<Capability> Capabilities { get; set; }
         public DbSet<Employer> Employers { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<Placement> Placements {get; set;}
         
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        public PlaDatContext() { }
+
+        public PlaDatContext(DbContextOptions<PlaDatContext> options)
+            : base(options) { } 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = @"EmptyConnectionString";
+                optionsBuilder.UseSqlServer(connectionString);
+            }
         }
         
-    }
-    
-    
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }  
 }
---
+++ api/Api/Models/PlaDatRepository.cs
@@ -0,0 +1,19 @@
+using Api.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    public class PlaDatRepository
+    {
+        private readonly PlaDatContext context;
+
+        public PlaDatRepository(PlaDatContext context)
+        {
+            this.context = context;
+        }
+
+        //Insert repository methods here
+    }
+}
