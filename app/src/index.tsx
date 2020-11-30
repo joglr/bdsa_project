86025a690418f3eff1d59cf4e5ac2bc9cd608b22
Jonas Røssum <jonas.g.roessum@gmail.com>
2020-11-30 15:07:54 +0100

    Add comments

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -21,7 +21,14 @@ ReactDOM.render(
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.unregister();
 
+// TODO: Display app version
+// TODO: Register service worker
+// TODO: Display update toast with reload button
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// TODO: Add web vitals reporting
+
