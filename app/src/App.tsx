a6edda9cf1aa28d01b36b8c5ce03fa8bf2318cac
Jonas Røssum <jonas.g.roessum@gmail.com>
2020-11-30 13:29:58 +0100

    Add placeholder app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Button, Grid, LinearProgress } from "@material-ui/core";
+import { ACTION_TYPE, Status, useStore } from "./store";
 
 function App() {
+  const [state] = useStore();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid
+      style={{
+        height: "100vh",
+      }}
+      container
+      direction="column"
+      justify="space-evenly"
+      alignItems="center"
+      alignContent="center"
+    >
+      <SubmitButton />
+      <span>State: {state.status}</span>
+      <br />
+      {state.status === Status.PENDING ? (
+        <LinearProgress
+          style={{
+            width: 100,
+          }}
+        />
+      ) : state.status === Status.SUCCESS ? (
+        "Result: " + JSON.stringify(state.response)
+      ) : (
+        <div />
+      )}
+    </Grid>
   );
+}
+
+interface SubmitButtonProps {}
+
+function SubmitButton(props: SubmitButtonProps) {
+  const [state, dispatch] = useStore();
+
+  async function submit() {
+    dispatch({
+      type: ACTION_TYPE.SUBMIT,
+      value: "hej",
+    });
+
+    await sleep(2000);
+    const response = await fetch("api.json");
+
+    if (response.ok && response.status >= 200 && response.status < 300) {
+      try {
+        dispatch({
+          type: ACTION_TYPE.SUCCEED,
+          response: await response.json(),
+        });
+      } catch (e) {
+        dispatch({
+          type: ACTION_TYPE.FAIL,
+          error: e.message,
+        });
+      }
+    } else {
+      dispatch({
+        type: ACTION_TYPE.FAIL,
+        error: response.statusText,
+      });
+    }
+  }
+  return (
+    <Grid item>
+      <Button
+        onClick={submit}
+        color="secondary"
+        disabled={state.status === Status.PENDING}
+        variant="text"
+      >
+        Submit
+      </Button>
+    </Grid>
+  );
+}
+
+function sleep(amount: number) {
+  return new Promise((resolve) => setTimeout(resolve, amount));
 }
 
 export default App;
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import App from "./App";
+import { StoreProvider } from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StoreProvider>
+      <App />
+    </StoreProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
