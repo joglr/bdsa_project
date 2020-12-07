829c7e34708d14d4af71278683cd4eb36451eca7
Jonas Røssum <jonas.g.roessum@gmail.com>
2020-12-07 11:42:38 +0100

    Signout should navigate to home

--- app/src/Settings.tsx
+++ app/src/Settings.tsx
@@ -1,21 +1,23 @@
 import { Button } from "@material-ui/core";
-import { RouteComponentProps } from "@reach/router";
+import { RouteComponentProps, useNavigate } from "@reach/router";
 import React from "react";
 import { Content } from "./components/util";
 import { ACTION_TYPE, useStore } from "./store";
 
 export default function Settings(_: RouteComponentProps) {
   const [, dispatch] = useStore();
+  const navigate = useNavigate();
   return (
     <Content>
       <Button
-        onClick={() =>
+        onClick={async () => {
+          await navigate("/");
           dispatch({
             type: ACTION_TYPE.CHANGE_USER,
             user: null,
             userType: null,
-          })
-        }
+          });
+        }}
       >
         Sign out
       </Button>
