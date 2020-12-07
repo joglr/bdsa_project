3f0b9641aa86551593215b40c046ff11725ecf55
emiljapelt <ejapelt@hotmail.com>
2020-12-07 11:24:45 +0100

    Fix reading single student not working

--- api/api.Models/Student/StudentRepository.cs
+++ api/api.Models/Student/StudentRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -7,192 +8,216 @@ using Microsoft.EntityFrameworkCore;
 
 namespace api.Models
 {
-  public class StudentRepository : IStudentRepository
-  {
-    private readonly IPlaDatContext context;
-
-    public StudentRepository(IPlaDatContext context)
+    public class StudentRepository : IStudentRepository
     {
-      this.context = context;
-    }
+        private readonly IPlaDatContext context;
 
-    public async Task<StudentReadDTO> ReadAsync(int id)
-    {
-      var studentQuery =
-          from s in context.Students
-          where s.Id == id
-          select new StudentReadDTO
-          {
-            Id = s.Id,
-            FirstName = s.FirstName,
-            LastName = s.LastName,
-            Email = s.Email,
-            PhoneNumber = s.PhoneNumber,
-            Capabilities =
-                  (from c in s.Capabilities
-                   select new CapabilityReadDTO
-                   {
-                     Id = c.Id,
-                     Name = c.Name,
-                     Description = c.Description
-                   }).ToList(),
-            Placements =
-                  (from p in s.Placements
-                   select new PlacementReadDTO
-                   {
-                     Id = p.Id,
-                     Title = p.Title,
-                     Description = p.Description,
-                     Location = p.Location,
-                     MinHours = p.MinHours,
-                     MaxHours = p.MaxHours,
-                     PlacementImage = p.PlacementImage,
-                     Employer = new EmployerReadDTO
-                     {
-                       Id = p.EmployerCompany.Id,
-                       CompanyName = p.EmployerCompany.CompanyName,
-                       CompanyDescription = p.EmployerCompany.CompanyDescription,
-                       CompanyImage = p.EmployerCompany.CompanyImage
-                     },
-                     Capabilities =
-                          (from c in s.Capabilities
-                           select new CapabilityReadDTO
-                           {
-                             Id = c.Id,
-                             Name = c.Name,
-                             Description = c.Description
-                           }).ToList()
-                   }).ToList()
-          };
-      return await studentQuery.FirstOrDefaultAsync();
-    }
-
-    public async Task<List<StudentReadDTO>> ReadAllAsync()
-    {
-      var studentQuery =
-          from s in context.Students
-          select new StudentReadDTO
-          {
-            Id = s.Id,
-            FirstName = s.FirstName,
-            LastName = s.LastName,
-            Email = s.Email,
-            PhoneNumber = s.PhoneNumber,
-            Capabilities =
-                  (from c in s.Capabilities
-                   select new CapabilityReadDTO
-                   {
-                     Id = c.Id,
-                     Name = c.Name,
-                     Description = c.Description
-                   }).ToList(),
-            Placements =
-                  (from p in s.Placements
-                   select new PlacementReadDTO
-                   {
-                     Id = p.Id,
-                     Title = p.Title,
-                     Description = p.Description,
-                     Location = p.Location,
-                     MinHours = p.MinHours,
-                     MaxHours = p.MaxHours,
-                     PlacementImage = p.PlacementImage,
-                     Employer = new EmployerReadDTO
-                     {
-                       Id = p.EmployerCompany.Id,
-                       CompanyName = p.EmployerCompany.CompanyName,
-                       CompanyDescription = p.EmployerCompany.CompanyDescription,
-                       CompanyImage = p.EmployerCompany.CompanyImage
-                     },
-                     Capabilities =
-                          (from c in p.Capabilities
-                           select new CapabilityReadDTO
-                           {
-                             Id = c.Id,
-                             Name = c.Name,
-                             Description = c.Description
-                           }).ToList()
-                   }).ToList()
-          };
-      return await studentQuery.ToListAsync();
-    }
-
-    public async Task<int> CreateAsync(StudentCreateDTO student)
-    {
-      var entity = new Student
-      {
-        FirstName = student.FirstName,
-        LastName = student.LastName,
-        Email = student.Email,
-        PhoneNumber = student.PhoneNumber
-      };
-
-      await context.Students.AddAsync(entity);
-
-      entity.Capabilities = MapCapabilities(student.Capabilities).Result;
-
-      await context.SaveChangesAsync();
-
-      return entity.Id;
-    }
-
-    public async Task<int> DeleteAsync(int id)
-    {
-      var studentsQuery =
-          from s in context.Students
-          where s.Id == id
-          select s;
-
-      if (!studentsQuery.Any()) return -1;
-      var foundStudent = await studentsQuery.FirstOrDefaultAsync();
-      context.Students.Remove(foundStudent);
-      await context.SaveChangesAsync();
-      return foundStudent.Id;
-    }
-
-    public async Task<int> UpdateAsync(int id, StudentUpdateDTO student)
-    {
-      var entity = await context.Students.FindAsync(id);
-
-      if (entity == null)
-      {
-        return -1;
-      }
-
-      foreach (int placementId in student.Placements)
-      {
-        var placementQuery = from p in context.Placements where p.Id == placementId select p;
-        if (await placementQuery.AnyAsync())
+        public StudentRepository(IPlaDatContext context)
         {
-          var placement = await placementQuery.FirstAsync();
-          System.Console.WriteLine("company: " + placement.EmployerCompany);
-          if (entity.Placements == null) entity.Placements = new List<Placement>();
-          entity.Placements.Add(placement);
+            this.context = context;
         }
-      }
 
-      entity.FirstName = student.FirstName;
-      entity.LastName = student.LastName;
-      entity.Email = student.Email;
-      entity.PhoneNumber = student.PhoneNumber;
+        public async Task<StudentReadDTO> ReadAsync(int id)
+        {
+            var studentQuery =
+                from s in context.Students
+                where s.Id == id
+                select new StudentReadDTO
+                {
+                    Id = s.Id,
+                    FirstName = s.FirstName,
+                    LastName = s.LastName,
+                    Email = s.Email,
+                    PhoneNumber = s.PhoneNumber,
+                    Capabilities =
+                        (from c in s.Capabilities
+                         select new CapabilityReadDTO
+                         {
+                             Id = c.Id,
+                             Name = c.Name,
+                             Description = c.Description
+                         }).ToList(),
+                    Placements =
+                        (from p in s.Placements
+                         select new PlacementReadDTO
+                         {
+                             Id = p.Id,
+                             Title = p.Title,
+                             Description = p.Description,
+                             Location = p.Location,
+                             MinHours = p.MinHours,
+                             MaxHours = p.MaxHours,
+                             PlacementImage = p.PlacementImage,
+                             Employer = new EmployerReadDTO
+                             {
+                                 Id = p.EmployerCompany.Id,
+                                 CompanyName = p.EmployerCompany.CompanyName,
+                                 CompanyDescription = p.EmployerCompany.CompanyDescription,
+                                 CompanyImage = p.EmployerCompany.CompanyImage
+                             },
+                             Capabilities =
+                                (from c in p.Capabilities
+                                 select new CapabilityReadDTO
+                                 {
+                                     Id = c.Id,
+                                     Name = c.Name,
+                                     Description = c.Description
+                                 }).ToList()
+                         }).ToList()
+                };
+            return await studentQuery.FirstAsync();
+        }
 
-      await context.SaveChangesAsync();
+        public async Task<List<StudentReadDTO>> ReadAllAsync()
+        {
+            var studentQuery =
+                from s in context.Students
+                select new StudentReadDTO
+                {
+                    Id = s.Id,
+                    FirstName = s.FirstName,
+                    LastName = s.LastName,
+                    Email = s.Email,
+                    PhoneNumber = s.PhoneNumber,
+                    Capabilities =
+                        (from c in s.Capabilities
+                         select new CapabilityReadDTO
+                         {
+                             Id = c.Id,
+                             Name = c.Name,
+                             Description = c.Description
+                         }).ToList(),
+                    Placements =
+                        (from p in s.Placements
+                         select new PlacementReadDTO
+                         {
+                             Id = p.Id,
+                             Title = p.Title,
+                             Description = p.Description,
+                             Location = p.Location,
+                             MinHours = p.MinHours,
+                             MaxHours = p.MaxHours,
+                             PlacementImage = p.PlacementImage,
+                             Employer = new EmployerReadDTO
+                             {
+                                 Id = p.EmployerCompany.Id,
+                                 CompanyName = p.EmployerCompany.CompanyName,
+                                 CompanyDescription = p.EmployerCompany.CompanyDescription,
+                                 CompanyImage = p.EmployerCompany.CompanyImage
+                             },
+                             Capabilities =
+                                (from c in p.Capabilities
+                                 select new CapabilityReadDTO
+                                 {
+                                     Id = c.Id,
+                                     Name = c.Name,
+                                     Description = c.Description
+                                 }).ToList()
+                         }).ToList()
+                };
+            return await studentQuery.ToListAsync();
+        }
 
-      return entity.Id;
+        /*public async Task<PlacementReadDTO> ReadRandomPlacement(int id)
+        {
+            var placementQuery =
+              (from p in context.Placements
+               select p).ToList();
+
+            var studentPlacementQuery =
+              (from s in context.Students
+               where s.Id == id
+               select s.Placements).ToList();
+
+            var studentPlacements = studentPlacementQuery.FirstOrDefault();
+
+            var nonappliedPlacements = placementQuery.Except(studentPlacements);
+
+            Random rand = new Random();
+            var selectedPlacement = nonappliedPlacements.ElementAt(rand.Next(nonappliedPlacements.Count()));
+
+            return new PlacementReadDTO
+            {
+              Id = selectedPlacement.Id
+            }
+        }*/
+
+        public async Task<int> CreateAsync(StudentCreateDTO student)
+        {
+            var entity = new Student
+            {
+                FirstName = student.FirstName,
+                LastName = student.LastName,
+                Email = student.Email,
+                PhoneNumber = student.PhoneNumber
+            };
+
+            await context.Students.AddAsync(entity);
+
+            entity.Capabilities = MapCapabilities(student.Capabilities).Result;
+
+            await context.SaveChangesAsync();
+
+            return entity.Id;
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            var studentsQuery =
+                from s in context.Students
+                where s.Id == id
+                select s;
+
+            if (!studentsQuery.Any()) return -1;
+            var foundStudent = await studentsQuery.FirstOrDefaultAsync();
+            context.Students.Remove(foundStudent);
+            await context.SaveChangesAsync();
+            return foundStudent.Id;
+        }
+
+        public async Task<int> UpdateAsync(int id, StudentUpdateDTO student)
+        {
+            var entity = await context.Students.FindAsync(id);
+
+            if (entity == null)
+            {
+                return -1;
+            }
+
+            foreach (int placementId in student.Placements)
+            {
+                var placementQuery = from p in context.Placements where p.Id == placementId select p;
+                if (await placementQuery.AnyAsync())
+                {
+                    var placement = await placementQuery.FirstAsync();
+                    System.Console.WriteLine("company: " + placement.EmployerCompany);
+                    if (entity.Placements == null) entity.Placements = new List<Placement>();
+                    entity.Placements.Add(placement);
+                }
+            }
+
+            entity.FirstName = student.FirstName;
+            entity.LastName = student.LastName;
+            entity.Email = student.Email;
+            entity.PhoneNumber = student.PhoneNumber;
+
+            await context.SaveChangesAsync();
+
+            return entity.Id;
+        }
+
+        private async Task<List<Capability>> MapCapabilities(List<int> capabilityList)
+        {
+            var capabilities = new List<Capability>();
+
+            foreach (int capabilityId in capabilityList)
+            {
+                var entity = await context.Capabilities.FirstOrDefaultAsync(c => c.Id == capabilityId);
+
+                if (entity != null) capabilities.Add(entity);
+            }
+
+            return capabilities;
+        }
     }
-
-    private async Task<List<Capability>> MapCapabilities(List<int> capabilityList)
-    {
-      var capabilities = new List<Capability>();
-
-      foreach (int capabilityId in capabilityList)
-      {
-        var entity = await context.Capabilities.FirstOrDefaultAsync(c => c.Id == capabilityId);
-
-        if (entity != null) capabilities.Add(entity);
-      }
-
-      return capabilities;
-    }
-  }
 }
