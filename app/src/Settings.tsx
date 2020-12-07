356af82210f615ef2b2a7e078372f0f545183de5
Jonas Røssum <jonas.g.roessum@gmail.com>
2020-12-07 11:33:00 +0100

    Add sign out button

--- app/src/Settings.tsx
+++ app/src/Settings.tsx
@@ -1,7 +1,24 @@
+import { Button } from "@material-ui/core";
 import { RouteComponentProps } from "@reach/router";
 import React from "react";
 import { Content } from "./components/util";
+import { ACTION_TYPE, useStore } from "./store";
 
 export default function Settings(_: RouteComponentProps) {
-  return <Content></Content>;
+  const [, dispatch] = useStore();
+  return (
+    <Content>
+      <Button
+        onClick={() =>
+          dispatch({
+            type: ACTION_TYPE.CHANGE_USER,
+            user: null,
+            userType: null,
+          })
+        }
+      >
+        Sign out
+      </Button>
+    </Content>
+  );
 }
